2de2173ae3452b8f755aca22804bcd11ee952cbf
slitherdmon <105375648+slitherdmon@users.noreply.github.com>
2025-04-21 10:10:45 -0500

    Create uv.config.js

---
+++ helper/uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/deionssearch/',
+    bare:'https://32867gd198764dg23vd23198746v4d69182736dvussis7.9882136.xyz/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/helper/uv/uv.handler.js',
+    bundle: '/helper/uv/uv.bundle.js',
+    config: '/helper/uv/uv.config.js',
+    sw: '/helper/uv/uv.sw.js',
+};
