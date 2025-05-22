4389f38a8308db4c5b52158b831fc266896a9044
slitherdmon <105375648+slitherdmon@users.noreply.github.com>
2025-05-22 09:29:22 -0500

    Update uv.config.js

--- helper/uv/uv.config.js
+++ helper/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/helper/math/',
-    bare:'https://ds.homeworkhelper.space/',
+    bare:'https://bare.benrogo.net/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/helper/uv/uv.handler.js',
