f1299c3e26906f8838686a1eca09803e069ce7c6
slitherdmon <105375648+slitherdmon@users.noreply.github.com>
2025-05-28 09:29:36 -0500

    Update uv.config.js

--- helper/uv/uv.config.js
+++ helper/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/helper/math/',
-    bare: 'https://dd-neon-chi.vercel.app/bare/',
+    bare: 'https://bare.benrogo.net',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/helper/uv/uv.handler.js',
